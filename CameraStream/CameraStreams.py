ebf08bbc4404fb4456dbd2fa031d15dcb71d3877
idenning2003 <idenning@iastate.edu>
2023-10-06 17:10:49 -0500

    Moving code to Jetson for further development

--- CameraStream/CameraStreams.py
+++ CameraStream/CameraStreams.py
@@ -13,13 +13,13 @@ import threading
 
 # Which port should be used for each camera.
 # The key is the camera ID and the value is the port to stream from.
-# To find the camera ID run TODO
-STREAM_PORTS = {
-    "i77tythguu": 5000,
-    "rdkuy5tgrd": 5001
+# To find the camera ID run "ls -l /dev/v4l/by-id/"
+DEFAULT_STREAM_PORTS = {
+    "usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index0": 5000,
+    "usb-HD_USB_Camera_HD_USB_Camera-video-index0": 5001
 }
-IMAGE_NAME = "cysar_camera_streamer"
-
+DEFAULT_IMAGE_NAME = "cysar_camera_streamer"
+DEFAULT_DOCKER_PATH = os.path.dirname(__file__)
 
 class CameraStreams:
     """
@@ -27,17 +27,20 @@ class CameraStreams:
 
     Args:
         stream_ports (dict): dict of the Camera ID to port. For example,
-            {TODO: 5000, TODO: 5001}
+            {usb-Camera-video-index0: 5000}
         keep_alive (bool): whether the camera stream containers 
             should be rebooted if they stop working 'True' for
             reboot the containter and keep the streams alive
+        build_path (str): 
     """
-    def __init__(self, stream_ports : dict = STREAM_PORTS, keep_alive : bool = True) -> None:
+    def __init__(self, stream_ports : dict = DEFAULT_STREAM_PORTS,
+                 keep_alive : bool = True,
+                 docker_path : str = DEFAULT_DOCKER_PATH
+                 ) -> None:
         self.keep_alive = keep_alive
         self.client = docker.from_env()
-        self.image_name = IMAGE_NAME
-        self.build_path = os.path.dirname(__file__)
-        self.docker_file_path = os.path.join(self.build_path, "Dockerfile")
+        self.image_name = DEFAULT_IMAGE_NAME
+        self.docker_path = docker_path
         self.container = None
         self.keep_alive_thread = None
         self.image = self._get_image()
@@ -62,13 +65,16 @@ class CameraStreams:
         """
         Return the builds the Docker image.
         """
-        return self.client.images.build(path=self.build_path, dockerfile=self.docker_file_path, quiet=False, rm=True, tag=self.image_name)
+        return self.client.images.build(path="TODO", 
+                                        dockerfile=self.docker_path, 
+                                        quiet=False, 
+                                        rm=True,
+                                        tag=self.image_name)
     
     def startup(self) -> None:
         """
         Starts the camera streams and initializes keep alive
         threads if that was enabled in constructor.
-
         TODO
         """
         self.container = self.client.containers.run(image=self.image.id, detach=True)
@@ -80,7 +86,6 @@ class CameraStreams:
     def shutdown(self) -> None:
         """
         Stop all the camera streams.
-
         TODO
         """
         if self.keep_alive_thread is threading.Thread:
@@ -105,7 +110,7 @@ class CameraStreams:
         ACCEPTABLE_STATUS = ["created", "running"]
         while self.keep_alive:
             try:
-                container.reload()
+                container.reload() # Updates the status of the container
             except: pass
             if container.status not in ACCEPTABLE_STATUS:
                 try:
