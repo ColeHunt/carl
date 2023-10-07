b34976a8cd375243a64ca6e4bc5834ff297a8796
idenning2003 <idenning@iastate.edu>
2023-10-06 23:50:30 -0500

    Basics are implemented

--- CameraStream/CameraStream.py
+++ CameraStream/CameraStream.py
@@ -1,4 +1,4 @@
-#/usr/bin/python3
+#!/usr/bin/python3
 
 """
 Camera.py
@@ -14,20 +14,18 @@ import os
 import threading
 import subprocess
 
+# ['/dev/v4l/by-id/usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index0 ../../video0', '/dev/v4l/by-id/usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index1 ../../video1', '/dev/v4l/by-id/usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index2 ../../video2', '/dev/v4l/by-id/usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index3 ../../video3']
+
 # Which port should be used for each camera.
 # The key is the camera ID and the value is the port to stream from.
 # To find the camera ID run "ls -l /dev/v4l/by-id/"
 DEFAULT_STREAM_PORTS = [
     {
-        "Cam_UUID": "usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index0",
+        "Cam_UUID": "/dev/v4l/by-id/usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index0",
         "Port": 5000
-    },
-    {
-        "Cam_UUID": "usb-HD_USB_Camera_HD_USB_Camera-video-index0",
-        "Port": 5001
     }
 ]
-    
+
 DEFAULT_IMAGE_NAME = "cysar_camera_streamer"
 DEFAULT_DOCKER_PATH = os.path.dirname(__file__)
 
@@ -85,11 +83,12 @@ class CameraStream:
         container = None
         while self._run_threads:
             camera = [cam[1] for cam in self.get_cameras() if cam[0] == CameraID]
+            print(self.get_cameras(), CameraID)
             if camera: # If the camera with ID exists
                 camera = camera[0] # Should only be one match anyways
-                self.client.containers.run(image=self._image.id,
+                self._client.containers.run(image=self._image.id,
                                            detach=True,
-                                           devices=[camera+":video0:rwm"],
+                                           devices=[camera+":/dev/video0:rwm"],
                                            ports={'5000/tcp': port})
                 input("Wait a minute")
             else:
@@ -101,8 +100,8 @@ class CameraStream:
         """
         self._run_threads = True
         for stream in self.stream_ports:
-            thread = threading.Thread(target=self._host_stream, args=(self.image, stream["Cam_UUID"], stream["Port"]))
-            self.host_threads += thread
+            thread = threading.Thread(target=self._host_stream, args=(stream["Cam_UUID"], stream["Port"]))
+            self._host_threads += [thread]
             thread.start()
 
     def shutdown(self) -> None:
@@ -119,15 +118,17 @@ class CameraStream:
 
         Example:
             [
-                ['/dev/v4l/by-id/usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index0', '../../video0'],
-                ['/dev/v4l/by-id/usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index1', '../../video1']
+                ['/dev/v4l/by-id/usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index0', '/dev/video0'],
+                ['/dev/v4l/by-id/usb-H264_USB_Camera_H264_USB_Camera_2020032801-video-index1', '/dev/video1']
             ]
         """
         cmd = "ls -ld /dev/v4l/by-id/* | tr -s \" \" | awk -F ' ' '{print $9,$11}'"
         ps = subprocess.Popen(cmd,shell=True,stdout=subprocess.PIPE,stderr=subprocess.STDOUT)
         output = ps.communicate()[0].decode('UTF-8')
         cameras = output.strip().split('\n')
-        return [cam.split(' ') for cam in cameras]
+        cameras = [cam.split(' ') for cam in cameras]
+        cameras = [[cam[0], "/dev/v4l/by-id/"+cam[1]] for cam in cameras]
+        return cameras
 
     def prune_containers(self):
         """
@@ -135,7 +136,7 @@ class CameraStream:
         """
         containers = self._client.containers.list(all=True)
         for container in containers:
-            if container.attrs['Image'] == self.image.id:
+            if container.attrs['Image'] == self._image.id:
                 container.remove(force=True)
 
 if __name__ == "__main__":
